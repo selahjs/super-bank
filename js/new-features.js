07a2c2d471a6927074f776d97c7c85342f084872
selahjs <sjemal99@gmail.com>
2023-05-01 12:50:11 +0300

    Reworked the year on the footer to get data from API

--- js/new-features.js
+++ js/new-features.js
@@ -1,5 +1,14 @@
-const currentYear = new Date().getFullYear();
-const yearElement = document.getElementById("year").innerHTML = currentYear;
+// let currentYearFromApi;
+fetch('http://worldtimeapi.org/api/timezone/America/New_York')
+    .then(res=> res.json())
+    .then(data=> getData(data.datetime.split('-')[0]))
+
+function getData(data){
+    document.getElementById("year").innerHTML = data;
+}
+//old code
+// const currentYear = new Date().getFullYear();
+// const yearElement = document.getElementById("year").innerHTML = currentYear;
 
 function welcomeLoggedUser() {
     let logged_member = JSON.parse(localStorage.getItem('logged_member'));
@@ -7,16 +16,25 @@ function welcomeLoggedUser() {
         document.getElementById('logged_member').innerHTML = `Welcome ${logged_member.name}`;
     }
 }
-function checkfund(total){
+function checkfund(total,cart,email){
     let logged_member = JSON.parse(localStorage.getItem('logged_member'));
     if (logged_member.deposit >= total) {
-        console.log('sufficient fund')
         let button = document.getElementById('btn');
-        button.innerHTML = "Succuss"
-        button.setAttribute("disabled","true")
-        document.getElementById('checkout').innerHTML = "Succussfully checked out!"
+        // button.innerHTML = "Succuss"
+        // button.setAttribute("disabled","true")
+        // document.getElementById('checkout').innerHTML = "Succussfully checked out!"
+        console.log(cart)
+        const orders = []
+        cart.forEach((item,i)=>{
+            orders.push({
+                productId:item.product.id,
+                member:email,
+                amount:item.product.productPrice,
+                date:new Date()
+            })
+        })
+        localStorage.setItem('orders', JSON.stringify(orders))
     }else{
-        console.log('not enough')
         document.getElementById('btn').innerHTML = "failed"
         document.getElementById('checkout').innerHTML = "Insufficient fund!"
     }
